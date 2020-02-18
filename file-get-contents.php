a89cc7573a55a31a5876fa827a3342ca287a7b20
hamzehXXX <32751335+hamzehXXX@users.noreply.github.com>
2020-02-18 18:15:09 +0330

    Update file-get-contents.php

--- file-get-contents.php
+++ file-get-contents.php
@@ -7,7 +7,7 @@ $limit  =   0;
 $url = 'https://swapi.co/api/planets';
 $content = false;
 
-while((!$content or $limit == 0) AND $limit < 4 ) {
+while(!$content AND $limit < 4 ) {
 
     $content = @file_get_contents($url);
     if($content) {
