c39816c944725b841c2e0596105d8e55d541c9af
hamzehXXX <32751335+hamzehXXX@users.noreply.github.com>
2020-02-18 18:16:20 +0330

    Update file-get-contents.php

--- file-get-contents.php
+++ file-get-contents.php
@@ -8,8 +8,9 @@ $limit  =   0;
 $url = 'https://swapi.co/api/planets';
 $content = false;
 
-while(!$content AND $limit < 4 ) {
+while(!$content AND $limit < 4 ) { // If content is not null or $limit exceeds 4, end the loop
 
+    // Make sure to suppress the warning by putting an error control operator (@) in front of the call to file_get_contents()
     $content = @file_get_contents($url);
     if($content) {
         echo 'Congrats. The connection was successful.';
